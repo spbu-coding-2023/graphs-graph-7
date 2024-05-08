04855bd61d533a2068bf43cdae6979ea4dff81fe
suvorovrain <rodion.suvorov.94@mail.ru>
2024-05-08 08:04:59 +0000

    feat: add weight and direction to edge

--- src/main/kotlin/graph/model/Edge.kt
+++ src/main/kotlin/graph/model/Edge.kt
@@ -2,6 +2,8 @@ package graph.model
 
 abstract class Edge<E, V> {
     abstract var element: E
+    open var weight: Long = 1L
+    open var isDirected: Boolean = false
     abstract val vertices: Pair<Vertex<V>, Vertex<V>>
     open fun incident(v: Vertex<V>) = v == vertices.first || v == vertices.second
 }
