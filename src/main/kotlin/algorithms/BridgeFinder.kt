55ef1045810a6a022f993774b0714bd4ce053ca3
suvorovrain <rodion.suvorov.94@mail.ru>
2024-05-11 12:46:22 +0000

    feat: create class for bridge finder algorithm

---
+++ src/main/kotlin/algorithms/BridgeFinder.kt
@@ -0,0 +1,20 @@
+package algorithms
+
+import graph.model.Graph
+
+class BridgeFinder<V>(graph: Graph<V>) {
+    val visitedVertices = mutableListOf<V>()
+    val timeIn = mutableListOf<V>()
+    val fUp = mutableListOf<V>()
+    val timer = 0L
+    fun findBridges(){
+        fun dfs(){
+
+        }
+
+        for (i in ){
+
+        }
+    }
+
+}
