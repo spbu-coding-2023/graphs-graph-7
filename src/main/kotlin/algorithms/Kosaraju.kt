57e7cce046b7663565a0a87a50b1faa38c617b90
Sem4kok <w1usis@mail.ru>
2024-05-13 01:12:10 +0300

    feat(algo): kosaraju dfs added

--- src/main/kotlin/algorithms/Kosaraju.kt
+++ src/main/kotlin/algorithms/Kosaraju.kt
@@ -6,4 +6,20 @@ class Kosaraju<V>(private val graph: Graph<V>) {
     private val used = hashMapOf<Int, Boolean>()
     private val order = mutableListOf<Int>()
     private val component = mutableListOf<Int>()
+
+    private fun dfs1(vertexID: Int) {
+        used[vertexID] = true
+        val vertex = graph.vertices[vertexID] ?: return
+        for (edgeID in vertex.incidentEdges) {
+            val edge = graph.edges[edgeID] ?: continue
+            val nextVertexID = if (vertexID == edge.vertices.first) edge.vertices.second else edge.vertices.first
+            if (used[nextVertexID] != true) {
+                dfs1(nextVertexID)
+            }
+        }
+        order.add(vertexID)
+    }
+
+
+
 }
