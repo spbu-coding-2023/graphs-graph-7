910dcd9309a827dbdb4ef671a6b0d9aace82d782
suvorovrain <rodion.suvorov.94@mail.ru>
2024-05-13 09:53:41 +0000

    feat: implement dao rows

--- src/main/kotlin/databases/sqlite/dao/vertices/Vertex.kt
+++ src/main/kotlin/databases/sqlite/dao/vertices/Vertex.kt
@@ -1,4 +1,13 @@
 package databases.sqlite.dao.vertices
 
-class Vertex {
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
+
+class Vertex(id: EntityID<Int>) : IntEntity(id) {
+
+    companion object : IntEntityClass<Vertex>(Vertices)
+
+    var element by Vertices.element
+    var community by Vertices.community
 }
