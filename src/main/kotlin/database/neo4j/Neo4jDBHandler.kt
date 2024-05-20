c22769328731d574303643d23e2cbff4aba8c6aa
Sem4kok <w1usis@mail.ru>
2024-05-20 16:04:17 +0300

    feat(db): Neo4jDBHandler added

---
+++ src/main/kotlin/database/neo4j/Neo4jDBHandler.kt
@@ -0,0 +1,21 @@
+package database.neo4j
+
+import graph.model.Graph
+
+class Neo4jHandler(private val repository: Neo4jRepository) {
+
+    fun saveGraphToNeo4j(graph: Graph) {
+        for (vertex in graph.getVertices()) {
+            repository.addVertex(vertex.id, vertex.data, vertex.community)
+        }
+
+        for (edge in graph.getEdges()) {
+            repository.addEdge(edge.vertices.first, edge.vertices.second, edge.weight, edge.id)
+        }
+    }
+
+    fun loadGraphFromNeo4j(): Graph {
+        val graph = repository.getGraph()
+        return graph
+    }
+}
