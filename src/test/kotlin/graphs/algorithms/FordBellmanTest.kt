ac3a6e5893aae36c3e7bcbbc7159842daa7e4dcc
suvorovrain <rodion.suvorov.94@mail.ru>
2024-05-22 12:36:22 +0000

    feat: add tests for Ford Bellman

--- src/test/kotlin/graphs/algorithms/FordBellmanTest.kt
+++ src/test/kotlin/graphs/algorithms/FordBellmanTest.kt
@@ -1,6 +1,5 @@
 package graphs.algorithms
 
-import algorithms.BridgeFinder
 import algorithms.FordBellman
 import model.graph.Graph
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -10,53 +9,97 @@ import org.junit.jupiter.api.Test
 
 class FordBellmanTest {
     private lateinit var graph: Graph
-    @BeforeEach
-    fun setup() {
-        graph = Graph().apply {
-            addVertex(1,"Thomas Shelby")
-            addVertex(2, "Andrew Tate")
-            addVertex(3, "Iakov")
-            addVertex(4, "John Shelby")
-            addVertex(5, "Tristan Tate")
-            addVertex(6, "Arthur Shelby")
-            addVertex(7, "Ryan Gosling")
 
-            addEdge(1, 2, 1,1)
-            addEdge(3, 4, 2,2)
-            addEdge(1, 3, 3,3)
-            addEdge(2, 4, 4,4)
-            addEdge(2, 5, 5,5)
-            addEdge(5, 7, 6,6)
-
-            addVertex(8, "Pudge")
-            addVertex(9,"Tiny")
-            addVertex(10, "Lycan")
-            addVertex(11,"Io")
-            addVertex(12,"Lion")
-            addVertex(13,"Sniper")
-            addVertex(14,"Roshan")
-
-            addEdge(14, 8, 7,7)
-            addEdge(14, 9, 8,8)
-            addEdge(14, 10, 9,9)
-            addEdge(14, 11, 10,10)
-            addEdge(14, 12, 11,11)
-            addEdge(14, 13, 12,12)
-
-            addEdge(14, 3, 0,13)
-        }
-    }
     @Test
     @DisplayName("Path without negative cycles")
     public fun PathWithoutNegativeCycles(){
+        graph = Graph().apply {
+            addVertex(1, "Thomas Shelby")
+            addVertex(2, "Andrew Tate")
+            addVertex(3, "Iakov")
+            addVertex(4, "John Shelby")
+            addVertex(5, "Arthur Shelby")
+            addEdge(1, 2, 3, 5)
+            addEdge(2, 3, 6, 2)
+            addEdge(1, 4, 2, 4)
+            addEdge(5, 3, 1, 3)
+            addEdge(1, 3, 5, 1)
+            addEdge(4, 5, 1, 6)
+        }
         val fordBellman = FordBellman(graph)
-        fordBellman.shortestPath()
+        fordBellman.shortestPath(1,3)
+        val resultVertices = fordBellman.resultPathVertices
+        val resultEdges = fordBellman.resultPathEdges
+        val resultCycles = fordBellman.cycleFlag
+        val resultConnectionFlag = fordBellman.disconnectedGraphFlag
+        val expectedCycles = false
+        val expectedEdges = listOf(3,6,4)
+        val expectedVertices = listOf(3,5,4,1)
+        val expectedConnectionFlag = false
+        assertEquals(expectedConnectionFlag, resultConnectionFlag)
+        assertEquals(expectedCycles, resultCycles)
+        assertEquals(expectedEdges, resultEdges)
+        assertEquals(expectedVertices, resultVertices)
+    }
+    @Test
+    @DisplayName("Path with negative cycles")
+    public fun PathWithNegativeCycles(){
+        graph = Graph().apply {
+            addVertex(1, "Thomas Shelby")
+            addVertex(2, "Andrew Tate")
+            addVertex(3, "Iakov")
+            addVertex(4, "John Shelby")
+            addVertex(5, "John Shelby")
 
-        val resultVertices = fordBellman.verticesPath
-        val resultEdges = fordBellman.edgesPath
-        val expectedEdges = listOf(1)
-        val expectedVertices = listOf(1)
+            addEdge(1, 2, -2, 3)
+            addEdge(2, 3, 1, 2)
+            addEdge(4, 1, -1, 1)
+            addEdge(3, 4, 1, 4)
+            addEdge(4, 5, 5, 5)
+        }
+        val fordBellman = FordBellman(graph)
+        fordBellman.shortestPath(1,5)
+        val resultVertices = fordBellman.resultPathVertices
+        val resultEdges = fordBellman.resultPathEdges
+        val resultCycles = fordBellman.cycleFlag
+        val resultConnectionFlag = fordBellman.disconnectedGraphFlag
+        val expectedCycles = true
+        val expectedEdges = listOf(3,1,4,2)
+        val expectedVertices = listOf(2,1,4,3)
+        val expectedConnectionFlag = false
+        assertEquals(expectedConnectionFlag, resultConnectionFlag)
+        assertEquals(expectedCycles, resultCycles)
+        assertEquals(expectedEdges, resultEdges)
+        assertEquals(expectedVertices, resultVertices)
+    }
+    @Test
+    @DisplayName("Disconnected graph")
+    public fun DisconnectedGraph(){
+        graph = Graph().apply {
+            addVertex(1, "Thomas Shelby")
+            addVertex(2, "Andrew Tate")
+            addVertex(3, "Iakov")
+            addVertex(4, "John Shelby")
+            addVertex(5, "John Shelby")
 
+            addEdge(1, 2, 2, 3)
+            addEdge(2, 3, 1, 2)
+            addEdge(4, 1, -1, 1)
+            addEdge(3, 4, 1, 4)
+
+        }
+        val fordBellman = FordBellman(graph)
+        fordBellman.shortestPath(1,5)
+        val resultVertices = fordBellman.resultPathVertices
+        val resultEdges = fordBellman.resultPathEdges
+        val resultCycles = fordBellman.cycleFlag
+        val resultConnectionFlag = fordBellman.disconnectedGraphFlag
+        val expectedCycles = false
+        val expectedEdges = listOf<Int>()
+        val expectedVertices = listOf<Int>()
+        val expectedConnectionFlag = true
+        assertEquals(expectedConnectionFlag, resultConnectionFlag)
+        assertEquals(expectedCycles, resultCycles)
         assertEquals(expectedEdges, resultEdges)
         assertEquals(expectedVertices, resultVertices)
     }
