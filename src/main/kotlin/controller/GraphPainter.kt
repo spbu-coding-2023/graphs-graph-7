31a54345ea52b6b95dd4d8c621515cd246a25074
Sem4kok <w1usis@mail.ru>
2024-05-26 21:41:32 +0300

    feat(ui): graph painting by community

--- src/main/kotlin/controller/GraphPainter.kt
+++ src/main/kotlin/controller/GraphPainter.kt
@@ -11,8 +11,8 @@ class GraphPainter(private val graph: Graph, private val graphViewModel: GraphVi
     private val communities = finder.findCommunities()
 
     fun paint() {
-        for (community in communities) {
-            val communityColor = generateRandomColor(community.hashCode())
+        for ((i, community) in communities.withIndex()) {
+            val communityColor = generateRandomColor(i * 123)
             for (vertexID in community) {
                 val currVertex = graph.vertices[vertexID]
                 graphViewModel.verticesView[currVertex]!!.color=communityColor
