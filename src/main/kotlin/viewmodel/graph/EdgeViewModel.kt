fa65531d0955dbef18084932550633d787401afd
dronshock <anbrew200212@gmail.com>
2024-05-29 20:52:52 +0300

    feat(databases): add work with CSV
    Add new data models for vertex and graph
    Add dependencies
    Change EdgeViewModel class

--- src/main/kotlin/viewmodel/graph/EdgeViewModel.kt
+++ src/main/kotlin/viewmodel/graph/EdgeViewModel.kt
@@ -1,12 +1,11 @@
 package viewmodel.graph
 
-import androidx.compose.runtime.State
 import model.graph.Edge
 
 class EdgeViewModel(
     val u: VertexViewModel,
     val v: VertexViewModel,
-    private val e: Edge,
+    val e: Edge,
 ) {
     val weight
         get() = e.weight.toString()
