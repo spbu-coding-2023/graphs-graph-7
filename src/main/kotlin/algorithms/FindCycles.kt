9d76ea1bc1096d76209a85b8d126eef940e26127
dronshock <anbrew200212@gmail.com>
2024-05-29 07:24:38 +0300

    fix(algo): remove some extra code and refactor

--- src/main/kotlin/algorithms/FindCycles.kt
+++ src/main/kotlin/algorithms/FindCycles.kt
@@ -3,46 +3,35 @@ package algorithms
 import model.graph.Graph
 import model.graph.Vertex
 import java.util.*
-import java.util.function.Consumer
 
-/*
-class FindCycles(graph: Graph) {
-    fun findCycles(graph: Graph): Int {
-        val bridgeFinder = BridgeFinder(graph)
-        val bridgeEdges = bridgeFinder.findBridges()
-        val edgesWithoutBridges = graph.edges.minus(bridgeEdges.toSet())
-        return 0
-    }
-}
-*/
 class AllCyclesInDirectedGraphJohnson {
-    var blockedSet: MutableSet<Vertex>? = null
-    var blockedMap: MutableMap<Vertex, Set<Vertex>?>? = null
-    var stack: Deque<Vertex?>? = null
-    var allCycles: MutableList<List<Vertex?>>? = null
+    private var blockedSet: MutableSet<Vertex> = mutableSetOf()
+    private var blockedMap: MutableMap<Vertex, Set<Vertex>?> = mutableMapOf()
+    private var stack: Deque<Vertex?> = LinkedList()
+    private var allCycles: MutableList<List<Vertex?>> = mutableListOf()
 
-    fun simpleCyles(graph: Graph): List<List<Vertex?>> {
+    fun simpleCycles(graph: Graph): List<List<Vertex?>> {
         blockedSet = HashSet<Vertex>()
         blockedMap = HashMap<Vertex, Set<Vertex>?>()
         stack = LinkedList<Vertex?>()
         allCycles = ArrayList<List<Vertex?>>()
-        var startIndex: Int = 1
-        val tarjan: TarjanStronglyConnectedComponent = TarjanStronglyConnectedComponent()
+        var startIndex = 1
+        val tarjan = TarjanStronglyConnectedComponent()
         while (startIndex <= graph.vertices.size) {
             val subGraph: Graph = createSubGraph(startIndex, graph)
             val sccs: List<Set<Vertex>> = tarjan.scc(subGraph)
             val maybeLeastVertex: Vertex? = leastIndexSCC(sccs, subGraph)
             if (maybeLeastVertex != null) {
                 val leastVertex: Vertex = maybeLeastVertex
-                blockedSet!!.clear()
-                blockedMap!!.clear()
+                blockedSet.clear()
+                blockedMap.clear()
                 findCyclesInSCG(graph,leastVertex, leastVertex)
                 startIndex = leastVertex.id + 1
             } else {
                 break
             }
         }
-        return allCycles!!
+        return allCycles
     }
 
     private fun leastIndexSCC(sccs: List<Set<Vertex>>, subGraph: Graph): Vertex? {
@@ -65,7 +54,7 @@ class AllCyclesInDirectedGraphJohnson {
         if (minVertex == null) {
             return null
         }
-        val graphScc: Graph = Graph()
+        val graphScc = Graph()
         graphScc.isDirected = true
         for ((i, edge) in subGraph.getEdges().withIndex()) {
             if ((subGraph.vertices[edge.vertices.first] in minScc) && (subGraph.vertices[edge.vertices.second] in minScc)) {
@@ -78,14 +67,14 @@ class AllCyclesInDirectedGraphJohnson {
     }
 
     private fun unblock(u: Vertex) {
-        blockedSet!!.remove(u)
-        if (blockedMap!![u] != null) {
-            blockedMap!![u]!!.forEach(Consumer<Vertex> { v: Vertex ->
-                if (blockedSet!!.contains(v)) {
+        blockedSet.remove(u)
+        if (blockedMap[u] != null) {
+            blockedMap[u]!!.forEach { v: Vertex ->
+                if (blockedSet.contains(v)) {
                     unblock(v)
                 }
-            })
-            blockedMap!!.remove(u)
+            }
+            blockedMap.remove(u)
         }
     }
 
@@ -95,53 +84,46 @@ class AllCyclesInDirectedGraphJohnson {
         currentVertex: Vertex,
     ): Boolean {
         var foundCycle = false
-        stack!!.push(currentVertex)
-        blockedSet!!.add(currentVertex)
+        stack.push(currentVertex)
+        blockedSet.add(currentVertex)
 
         for (e in graph.edges.filterKeys{it in currentVertex.incidentEdges}.values) {
             val neighbor: Vertex = graph.vertices[e.vertices.second]!!
-            //if neighbor is same as start vertex means cycle is found.
-            //Store contents of stack in final result.
             if (neighbor === startVertex) {
                 val cycle: MutableList<Vertex?> = ArrayList<Vertex?>()
-                stack!!.push(startVertex)
-                cycle.addAll(stack!!)
-                Collections.reverse(cycle)
-                stack!!.pop()
-                allCycles!!.add(cycle)
+                stack.push(startVertex)
+                cycle.addAll(stack)
+                cycle.reverse()
+                stack.pop()
+                allCycles.add(cycle)
                 foundCycle = true
-            } //explore this neighbor only if it is not in blockedSet.
-            else if (!blockedSet!!.contains(neighbor)) {
+            }
+            else if (!blockedSet.contains(neighbor)) {
                 val gotCycle =
                     findCyclesInSCG(graph,startVertex, neighbor)
                 foundCycle = foundCycle || gotCycle
             }
         }
-        //if cycle is found with current vertex then recursively unblock vertex and all vertices which are dependent on this vertex.
         if (foundCycle) {
-            //remove from blockedSet  and then remove all the other vertices dependent on this vertex from blockedSet
             unblock(currentVertex)
         } else {
-            //if no cycle is found with current vertex then don't unblock it. But find all its neighbors and add this
-            //vertex to their blockedMap. If any of those neighbors ever get unblocked then unblock current vertex as well.
             for (e in graph.edges.filterKeys{it in currentVertex.incidentEdges}.values) {
                 val w: Vertex = graph.vertices[e.vertices.second]!!
                 val bSet: MutableSet<Vertex> = getBSet(w)
                 bSet.add(currentVertex)
             }
         }
-        //remove vertex from the stack.
-        stack!!.pop()
+        stack.pop()
         return foundCycle
     }
 
     private fun getBSet(v: Vertex): MutableSet<Vertex> {
-        return (blockedMap?.computeIfAbsent(v
+        return (blockedMap.computeIfAbsent(v
         ) { HashSet<Vertex>() } as MutableSet<Vertex>?)!!
     }
 
     private fun createSubGraph(startVertex: Int, graph: Graph): Graph {
-        val subGraph: Graph = Graph()
+        val subGraph = Graph()
         subGraph.isDirected = true
         for ((i, edge) in graph.getEdges().withIndex()) {
             if (edge.vertices.first >= startVertex && edge.vertices.second >= startVertex) {
@@ -152,40 +134,36 @@ class AllCyclesInDirectedGraphJohnson {
         }
         return subGraph
     }
-
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            val johnson = AllCyclesInDirectedGraphJohnson()
-            val graph: Graph = Graph()
-            graph.isDirected = true
-            for (i in 1..9){
-                graph.addVertex(i,"")
-            }
-            graph.addEdge(1, 2, edgeID = 0)
-            graph.addEdge(1, 8, edgeID = 1)
-            graph.addEdge(1, 5, edgeID = 2)
-            graph.addEdge(2, 9, edgeID = 3)
-            graph.addEdge(2, 7, edgeID = 4)
-            graph.addEdge(2, 3, edgeID = 5)
-            graph.addEdge(3, 1, edgeID = 6)
-            graph.addEdge(3, 2, edgeID = 7)
-            graph.addEdge(3, 6, edgeID = 8)
-            graph.addEdge(3, 4, edgeID = 9)
-            graph.addEdge(6, 4, edgeID = 10)
-            graph.addEdge(4, 5, edgeID = 11)
-            graph.addEdge(5, 2, edgeID = 12)
-            graph.addEdge(8, 9, edgeID = 13)
-            graph.addEdge(9, 8, edgeID = 14)
-
-            val allCycles: List<List<Vertex?>> = johnson.simpleCyles(graph)
-            val joiner = StringJoiner("->")
-            allCycles.forEach { cycle: List<Vertex?> ->
-                cycle.forEach { vertex: Vertex? ->
-                    joiner.add("${vertex?.id}")
-                }
-                println(joiner)
-            }
-        }
-    }
 }
+//fun main(args: Array<String>) {
+//    val johnson = AllCyclesInDirectedGraphJohnson()
+//    val graph = Graph()
+//    graph.isDirected = true
+//    for (i in 1..9){
+//        graph.addVertex(i,"")
+//    }
+//    graph.addEdge(1, 2, edgeID = 0)
+//    graph.addEdge(1, 8, edgeID = 1)
+//    graph.addEdge(1, 5, edgeID = 2)
+//    graph.addEdge(2, 9, edgeID = 3)
+//    graph.addEdge(2, 7, edgeID = 4)
+//    graph.addEdge(2, 3, edgeID = 5)
+//    graph.addEdge(3, 1, edgeID = 6)
+//    graph.addEdge(3, 2, edgeID = 7)
+//    graph.addEdge(3, 6, edgeID = 8)
+//    graph.addEdge(3, 4, edgeID = 9)
+//    graph.addEdge(6, 4, edgeID = 10)
+//    graph.addEdge(4, 5, edgeID = 11)
+//    graph.addEdge(5, 2, edgeID = 12)
+//    graph.addEdge(8, 9, edgeID = 13)
+//    graph.addEdge(9, 8, edgeID = 14)
+//
+//    val allCycles: List<List<Vertex?>> = johnson.simpleCycles(graph)
+//    val joiner = StringJoiner("->")
+//    allCycles.forEach { cycle: List<Vertex?> ->
+//        cycle.forEach { vertex: Vertex? ->
+//            joiner.add("${vertex?.id}")
+//        }
+//        println(joiner)
+//    }
+//}
--- src/main/kotlin/algorithms/TarjanStrongConnecedComponent.kt
+++ src/main/kotlin/algorithms/TarjanStrongConnecedComponent.kt
@@ -5,18 +5,6 @@ import model.graph.Vertex
 import java.util.*
 import kotlin.math.min
 
-
-/**
- * Date 08/16/2015
- * @author Tushar Roy
- *
- * Find strongly connected components of directed graph.
- *
- * Time complexity is O(E + V)
- * Space complexity  is O(V)
- *
- * Reference - https://en.wikipedia.org/wiki/Tarjan%27s_strongly_connected_components_algorithm
- */
 class TarjanStronglyConnectedComponent {
     private var visitedTime: MutableMap<Vertex, Int> = mutableMapOf()
     private var lowTime: MutableMap<Vertex, Int> = mutableMapOf()
@@ -27,7 +15,6 @@ class TarjanStronglyConnectedComponent {
     private var time = 0
 
     fun scc(graph: Graph): List<Set<Vertex>> {
-        //start from any vertex in the graph.
         for (vertex in graph.getVertices()) {
             if (visited.contains(vertex)) {
                 continue
@@ -47,22 +34,15 @@ class TarjanStronglyConnectedComponent {
         onStack.add(vertex)
 
         for (child in vertex.adjacentVertices) {
-            //if child is not visited then visit it and see if it has link back to vertex's ancestor. In that case update
-            //low time to ancestor's visit time
             if (child !in visited) {
                 sccUtil(child)
-                //sets lowTime[vertex] = min(lowTime[vertex], lowTime[child]);
                 lowTime[vertex] = min(lowTime[vertex]!!, lowTime[child]!!)
-            } //if child is on stack then see if it was visited before vertex's low time. If yes then update vertex's low time to that.
+            }
             else if (child in onStack) {
-                //sets lowTime[vertex] = min(lowTime[vertex], visitedTime[child]);
                 lowTime[vertex] = min(lowTime[vertex]!!, visitedTime[child]!!)
             }
         }
 
-        //if vertex low time is same as visited time then this is start vertex for strongly connected component.
-        //keep popping vertices out of stack still you find current vertex. They are all part of one strongly
-        //connected component.
         if (visitedTime[vertex] === lowTime[vertex]) {
             val stronglyConnectedComponent: MutableSet<Vertex> = HashSet<Vertex>()
             var v: Vertex
