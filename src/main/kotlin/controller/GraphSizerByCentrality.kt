bf9c81e81cf238eaf682ef2e8b47858304a1951a
Andrew Strelnikov <a.strelnikov@yadro.com>
2024-10-31 13:30:51 +0300

    fix(keyValues): add instant redrawing of graph

--- src/main/kotlin/controller/GraphSizerByCentrality.kt
+++ src/main/kotlin/controller/GraphSizerByCentrality.kt
@@ -1,5 +1,6 @@
 package controller
 
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
 import model.graph.Graph
 import model.keyVertices.GraphBetweennessCentrality
@@ -8,10 +9,11 @@ import viewmodel.graph.GraphViewModel
 class GraphSizerByCentrality(private val graph: Graph, private val graphViewModel: GraphViewModel) {
     private val algoInitialize = GraphBetweennessCentrality()
     private val centrality = algoInitialize.getKeyVertices(graph)
-
+    
     fun changeSize() {
         for (vertex in centrality.keys) {
             graphViewModel.verticesView[vertex]!!.radius = centrality[vertex]!!.dp
+            graphViewModel.verticesView[vertex]!!.x += 1.dp
         }
     }
 }
